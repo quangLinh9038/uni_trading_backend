87ec0a24735f142ba8a9d2b572769f5799767d09
witm15 <s3697272@rmit.edu.vn>
2021-05-23 13:30:31 +0700

    [UPDATE] change GET methods product
    Change to return a list

--- src/main/java/com/rmit/trading_backend/repository/ProductRepository.java
+++ src/main/java/com/rmit/trading_backend/repository/ProductRepository.java
@@ -3,16 +3,19 @@ package com.rmit.trading_backend.repository;
 import com.rmit.trading_backend.model.product.Category;
 import com.rmit.trading_backend.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    Optional<Product> findProductByNameContaining(String name);
+    List<Product> findProductByNameContaining(String name);
 
-    Optional<Product> findProductsByBrandContaining(String brand);
+    List<Product> findProductsByBrandContaining(String brand);
 
-    Optional<Product> findProductsByCategory(Category category);
+    //    Optional<Product> findProductsByCategory(Category category);
+    List<Product> findAllProductByCategoryId(@Param("id")int categoryID);
+    List<Product> findAllProductByCategoryName(@Param("name")String categoryName);
 
 }
